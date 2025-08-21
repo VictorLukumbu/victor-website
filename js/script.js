424e2567705a9b6b52da0eb7d4b62ca4aaa216f4
VictorLukumbu <vlukumbu@gmail.com>
2025-08-21 15:40:18 +0200

    Add files via upload
    uploading all my html files folders and files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Here document.addEventListener will wait until the page is fully loaded before 
+// running code.
+// And console.log will log a message to the browser console (for testing/debugging) 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Victor’s site is live and kicking!");
+  });
